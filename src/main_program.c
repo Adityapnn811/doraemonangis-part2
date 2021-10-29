5fb8ad2dd81a41e570fe770c274f4b2df49a2dd1
Nadia Mareta <82803405+KorbanFidas2A@users.noreply.github.com>
2021-10-29 11:50:51 +0700

    Add files via upload

--- src/main_program.c
+++ src/main_program.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include "wordmachine.h"
 #include "game_header.h"
 
 boolean endWord;
 Word currentWord;
-/* nanti diinclude semua yang dibutuhin disini */
+/* jangan include header di sini, tapi include di game_header.h */
 
 int main(){
     boolean start = true;
@@ -20,6 +19,7 @@ int main(){
         printf("i. NEW GAME\n");
         printf("ii. LOAD GAME\n");
         printf("iii. EXIT\n");
+        printf("iv. HELP\n");
         printf("\n");
         printf("Silahkan input opsi yang di atas: ");
         while(!endWord){
@@ -27,19 +27,38 @@ int main(){
                 /* pengaturan kondisi pilihan user */
             if(currentWord.contents[0] == 'i' && endWord == true && currentWord.length == 1){
                     /* konfigurasi new game */
+
+
+                    /* fungsi */
             }else if(currentWord.contents[0] == 'i' && currentWord.contents[1] == 'i' && currentWord.length == 2 && endWord == true){
                     /* fungsi load */
+
+
+                    /* fungsi */
             }else if(currentWord.contents[0] == 'i' && currentWord.contents[1] == 'i' && currentWord.contents[2] == 'i' && endWord == true && currentWord.length == 3){
+                /* keluar */
+                
                 valid = !valid;
                 printf("Selamat bertemu kembali!\n");
                 exit(0);
-            }else if(endWord == true){
+            }else if(endWord){
                 /* ini masi infinity loop nanti mau dipikir ulang gimana caranya biar ga infinity loop */
                 /* printf("Input salah. Silahkan ulangi input hingga sesuai.\n");
                 endWord = !endWord;
                 startWord(); */
-                
+                if(currentWord.contents[0] == 'i' && currentWord.contents[1] == 'v' && currentWord.length == 2){
+                    printf("\n");
+                    show_help();
+                    /* ini nantinya bakal ngulang inputnya sampe dia milih salah satu di atas. Tapi untuk sementara
+                    diberentiin dulu krn ku masih make wordmachine yang lama (infinity loop) */
+                    valid = !valid;
+                }else{
+                    endWord = !endWord;
+                    startWord();
+                }
             }
         }
     }
+
+    return 0;
 }
