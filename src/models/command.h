ffa9f58eb1592c570f8406ef928b668d16e8400e
Nadia Mareta <82803405+KorbanFidas2A@users.noreply.github.com>
2021-10-29 10:58:42 +0700

    Add files via upload
    array buat command nanti

---
+++ src/models/command.h
@@ -0,0 +1,25 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include "boolean.h"
+
+/* berisi command-command yang akan digunakan */
+boolean move();
+
+boolean pick_up();
+
+boolean drop_off();
+
+boolean map();
+
+boolean to_do();
+
+boolean in_progress();
+
+boolean buy();
+
+boolean inventory();
+
+boolean help();
+
+#endif
