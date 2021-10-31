2e34daa80da6e8ec44ac159e959fffa0a22e5713
Nadia Mareta <82803405+KorbanFidas2A@users.noreply.github.com>
2021-10-31 23:27:28 +0700

    Update Makefile.mk

--- build/Makefile.mk
+++ build/Makefile.mk
@@ -1,1 +1,12 @@
-
+a.out:main_program.o command.o charmachine.o wordmachine.o help.o
+	gcc main_program.o command.o charmachine.o wordmachine.o help.o
+main_program.o:main_program.c
+	gcc -c main_program.c
+command.o:command.c
+	gcc -c command.c
+charmachine.o:charmachine.c
+	gcc -c charmachine.c
+wordmachine.o:wordmachine.c
+	gcc -c wordmachine.c
+help.o:help.c
+	gcc -c help.c
