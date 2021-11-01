daeb69690d80324622576d6a8d04912b2c5c51e0
Nadia Mareta <82803405+KorbanFidas2A@users.noreply.github.com>
2021-11-01 11:10:22 +0700

    Update game_header.h

--- src/game_header.h
+++ src/game_header.h
@@ -1,9 +1,9 @@
 #ifndef GAMEHEADER_H
 #define GAMEHEADER_H
 
-#include "./include/command.h"
-#include "./include/help.h"
-#include "./src/wordmachine.h"
+#include "./adt/command.h"
+#include "./models/help.h"
+#include "./adt/wordmachine.h"
 
 /* include header masing - masing di sini */
 
