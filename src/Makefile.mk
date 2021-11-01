d80d3d80a3dc822d65630e902bbb32e281f5fc67
Nadia Mareta <82803405+KorbanFidas2A@users.noreply.github.com>
2021-11-01 11:05:19 +0700

    ganti makefile

--- src/Makefile.mk
+++ src/Makefile.mk
@@ -1,28 +1,27 @@
-# target macros
+
 TARGET := game.exe
 MAIN_SRC := main_program.c
 
-# compile macros
+
 DIRS := adt adt_modified models
 OBJS := 
 
-# intermedia compile macros
 ALL_OBJS := 
 CLEAN_FILES := $(TARGET) $(OBJS)
 DIST_CLEAN_FILES := $(OBJS)
 
-# recursive wildcard
+
 rwildcard=$(foreach d,$(wildcard $(addsuffix *,$(1))),$(call rwildcard,$(d)/,$(2))$(filter $(subst *,%,$(2)),$(d)))
 
-# default target
+
 default: show-info all
 
-# non-phony targets
+
 $(TARGET): build-subdirs $(OBJS) find-all-objs
 	@echo -e "\t" CC $(CCFLAGS) $(ALL_OBJS) $(MAIN_SRC) -o $@
 	@$(CC) $(CCFLAGS) $(ALL_OBJS) $(MAIN_SRC) -o $@
 
-# phony targets
+
 .PHONY: all
 all: $(TARGET)
 	@echo Target $(TARGET) build finished.
@@ -37,7 +36,7 @@ distclean: clean-subdirs
 	@echo CLEAN $(DIST_CLEAN_FILES)
 	@rm -f $(DIST_CLEAN_FILES)
 
-# phony funcs
+
 .PHONY: find-all-objs
 find-all-objs:
 	$(eval ALL_OBJS += $(call rwildcard,$(DIRS),*.o))
@@ -46,7 +45,7 @@ find-all-objs:
 show-info:
 	@echo Building Project
 
-# need to be placed at the end of the file
+
 mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
 export PROJECT_PATH := $(patsubst %/,%,$(dir $(mkfile_path)))
 export MAKE_INCLUDE=$(PROJECT_PATH)/config/make.global
