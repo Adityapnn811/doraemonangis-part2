6a4bc1fb06112194c82cd35883bbf903b1e2ec19
Hilya Fadhilah Imania <hilyafadhilah@gmail.com>
2021-11-17 19:37:34 +0700

    fix load.h

--- src/modules/load.h
+++ src/modules/load.h
@@ -4,6 +4,6 @@
 #include "../game_header.h"
 
 void saveGame(Player player, Inventory inv, Tas bag, TDList todo, Speedboost boost);
-void loadGame(Config conf, Player *player, Inventory *inv, Tas *bag, TDList *todo, Speedboost *boost);
+void loadGame(DaftarPesanan *pesanans, Player *player, Inventory *inv, Tas *bag, TDList *todo, Speedboost *boost);
 
 #endif
